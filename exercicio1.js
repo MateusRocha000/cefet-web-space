f24f1819eebc233b0804f8a1856211106ba623d5
MateusRocha000 <mateusrochagoncalves@gmail.com>
2018-04-19 12:41:53 -0300

    Terminar ex2

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,23 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let parag = document.getElementsByClassName("botao-expandir-retrair");
+
+
+for(let i in parag)
+{
+	parag[i].addEventListener("click", display);
+	
+}
+
+function display()
+{
+	if(this.innerHTML === "+")
+	{
+		this.innerHTML = "-";
+	}
+	else
+	{
+		this.innerHTML = "+";
+	}
+	this.parentElement.classList.toggle("expandido");
+}
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,29 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let prox = document.getElementById("proximo");
+let ant = document.getElementById("anterior");
+
+prox.addEventListener("click", galery(1));
+ant.addEventListener("click", galery(-1));
+
+ind = 0;
+
+function galery(value)
+{
+	if(value === 1)
+	{
+		ind = (ind + value);
+		if(ind > todasAsImagens.length)
+			ind = ind - todasAsImagens.length;
+	}
+	if(value === -1)
+	{
+		ind = (ind - value)
+		if(ind < 0)
+			ind = ind + todasAsImagens.length;
+	}
+
+	document.getElementById('slide').src = servidorDasImagens+todasAsImagens[ind];
+}
